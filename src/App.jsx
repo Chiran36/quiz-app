69636f812571a0b474f2e619107c3c8af128ad85
Chiranjivi Khatri <143805001+Chiran36@users.noreply.github.com>
2026-08-18 07:57:04 +0545

    Added the functionality for finish screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,124 @@
-import React from 'react'
-import Header from './components/Header'
-import { useEffect, useReducer } from 'react';
-import Loader from './components/Loader';
-import Error from './components/Error';
-import Main1 from './components/Main1';
-import StartScreen from './components/StartScreen';
-import Question from './components/Question';
+import React from "react";
+import Header from "./components/Header";
+import { useEffect, useReducer } from "react";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import Main1 from "./components/Main1";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
 
 const initialState = {
-  questions:[],
+  questions: [],
 
   // 'loading','error','active','ready','finished'
-  status:'loading',
-  index:0
-}
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
 
-function reducer(state,action){
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload == question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
 
-  switch(action.type){
-    case 'dataReceived':
-      return {...state,questions:action.payload,status:'ready'}
-    case 'dataFailed':
-      return {...state,status:'error'}
-    case 'start':
-      return {...state,status:'active'}
-
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.highScore > state.points ? state.highScore : state.points,
+      };
     default:
-      throw new Error("unknown action")
+      throw new Error("unknown action");
   }
-
 }
-
 
 export default function App() {
-
-  const [{questions,status,index},dispatch] = useReducer(reducer,initialState);
+  const [{ questions, status, index, answer, points,highScore }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const numQuestions = questions.length;
 
-  useEffect(function(){
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0,
+  );
 
-    async function fetchData(){
-      
-      try{
+  useEffect(function () {
+    async function fetchData() {
+      try {
         const res = await fetch(`http://localhost:8000/questions`);
         const data = await res.json();
-        dispatch({type:'dataReceived', payload:data})
-
-        
-
-      }catch(err){
-             dispatch({type:'dataFailed'});
-      }finally{
-
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      } finally {
       }
-      
-
     }
     fetchData();
-
-  },[])
-
+  }, []);
 
   return (
     <div className="app">
-      <Header/>
+      <Header />
       <Main1>
-        {status === 'loading' && <Loader/>}
-        {status === 'error' && <Error/>}
-        {status === 'loading' && <Loader/>}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-        {status === 'active' && <Question question={questions[index]}/>}
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "loading" && <Loader />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              answer={answer}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+
+            <Question
+              question={questions[index]}
+              answer={answer}
+              dispatch={dispatch}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highScore={highScore} />
+        )}
       </Main1>
-      
     </div>
-  )
+  );
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function options({question}) {
+export default function options({ question, answer, dispatch }) {
+  const hasAnswered = answer != null;
   return (
     <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>{option}</button>
-        ))}
-      </div>
-  )
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          disabled={hasAnswered}
+          key={option}
+          onClick={() => {
+            dispatch({ type: "newAnswer", payload: index });
+          }}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
